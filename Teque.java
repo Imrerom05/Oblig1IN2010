9037afb80e07a7dee0d0838a4b17a60438bc155a
llenamklenna <makoa030@gmail.com>
2026-09-02 11:46:29 +0200

    new file

---
+++ Teque.java
@@ -0,0 +1,18 @@
+public class Teque {
+    
+    public void push_back(int x) {
+        
+    }
+
+    public void push_front(int x) {
+
+    }
+
+    public void push_middle(int x) {
+
+    }
+
+    public int get(int i) {
+
+    }
+}
