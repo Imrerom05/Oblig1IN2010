d15fff5c73a0281d1ba2bc3e4ec96379c5196073
imrer <imrer@uio.no>
2026-09-02 12:15:11 +0200

    Teque

--- Teque.java
+++ Teque.java
@@ -1,18 +1,45 @@
+import ArrayList;
+
+
 public class Teque {
+
+    new ArrayList<Integer> right = new ArrayList<>();
+    new ArrayList<Integer> left = new ArrayList<>();
+
     
     public void push_back(int x) {
-        
+        right.add(x);
+        fix();
     }
 
     public void push_front(int x) {
-
+        left.add(0, x);
+        fix();
     }
 
     public void push_middle(int x) {
-
+        if (left.size() >= rigth.size()) {
+            left.add();
+        } else {
+            right.add(0, x);
+        }
     }
 
     public int get(int i) {
+        if (i >= left.size){
+            return right.get(i - left.size);
+        } else {
+            return left.get(i);
+        }
+    }
 
+    public void fix() {
+        if (left.size() > right.size() + 1) {
+            right.add(0, left.get(left.size() - 1));
+            left.remove(left.size() - 1);
+        } else if (right.size() > left.size() +1) {
+            left.add(right.get(0));
+            right.remove(0);
+        }
     }
 }
